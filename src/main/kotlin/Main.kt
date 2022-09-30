a8b20d24a2a9a8d14b0d53ace1f3d81584509385
DmitryEntwickler <chervontsev@yahoo.de>
2022-09-30 12:09:21 +0200

    Alles suaber gemacht

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,3 @@
-fun main(args: Array<String>) {
+fun main() {
     println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-}
-
-fun br1(){
-    println("heir branch1")
-}
-
-fun br2(){
-    println("heir branch2")
-    println("und heir branch2")
-}
-
-fun br3(){
-    println("branch3")
-    println("branch3-2")
-}
-
-fun br4(){
-    println("branch4")
-    println("br4")
-}
-
-
-fun hotfix() {
-    println("hotfix")
-    println("hotfix")
-}
-
-fun br5(){
-    println("branch5")
-
 }
